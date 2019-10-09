36c1ba548c9887698cedf8eacecc0583a80c193b
CaJR8023 <1206303099@qq.com>
2019-10-10 00:53:24 +0800

    todo

---
+++ index-gather-store-service/src/main/java/cn/my/trend/job/IndexDataSyncJob.java
@@ -0,0 +1,36 @@
+package cn.my.trend.job;
+
+import java.util.List;
+
+import cn.hutool.core.date.DateUtil;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import cn.my.trend.pojo.Index;
+import cn.my.trend.service.IndexDataService;
+import cn.my.trend.service.IndexService;
+
+
+public class IndexDataSyncJob extends QuartzJobBean {
+	
+
+	@Autowired
+	private IndexService indexService;
+
+	@Autowired
+	private IndexDataService indexDataService;
+	
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		System.out.println("定时启动：" + DateUtil.now());
+		List<Index> indexes = indexService.fresh();
+		for (Index index : indexes) {
+			 indexDataService.fresh(index.getCode());
+		}
+		System.out.println("定时结束：" + DateUtil.now());
+
+	}
+
+}
